43f0aee2581d9da4688c9f48db04b4add88dcb98
MRyzhylo <playa77777@gmail.com>
2019-05-17 15:54:14 +0300

    create screen resolution check

--- main.js
+++ main.js
@@ -74,4 +74,91 @@ function Burger(data, dataDop, nameId) {
     this.button2.onclick = this.changeAddItems;
 }
 
-new Burger(menuArr, dopMenu, '#menu_block');
+function AdaptiveBurger (data, dataDop, nameId) {
+    this.container = document.querySelector(nameId);
+    this.container2 = document.createElement('div');
+    this.container.appendChild(this.container2);
+    this.container2.id = 'adaptive_menu';
+    this.div = document.createElement('div');
+    this.div.id = 'admenu_block';
+    this.ul = document.createElement('ul');
+    this.button = document.createElement('button');
+    this.button.innerHTML = 'Change position';
+    this.button.className = 'adchange_button'
+    this.button2 = document.createElement('button');
+    this.button2.innerHTML = 'Change additional items';
+    this.button2.className = 'adchange_button'
+    this.container2.appendChild(this.button);
+    this.container2.appendChild(this.button2);
+    this.div.appendChild(this.ul);
+    this.container.appendChild(this.div);
+    this.ul.className = 'adwrap_block';
+    this.data = data;
+    this.dataDop = dataDop;
+    for (let menu of data) {
+        this.li = document.createElement('li');
+        this.li.className = 'adwrap';
+        this.a = document.createElement('a');
+        this.li.appendChild(this.a);
+        this.a.href = '#';
+        this.a.innerHTML = menu;
+        this.ul.appendChild(this.li);
+    }
+
+    this.styleDiv = document.createElement('div');
+    this.styleDiv.id = 'charters_block'
+    this.styleDiv.className = 'adstyle_block'
+
+    this.menuList = function (event) {
+        if (!event.target.classList.contains('adwrap')) return;
+        for (let name of dataDop) {
+            this.li = document.createElement('li');
+            this.li.className = 'adsmall_wrap';
+            this.a = document.createElement('a');
+            this.li.appendChild(this.a);
+            this.a.href = '#';
+            this.a.innerHTML = name;
+            event.target.appendChild(this.styleDiv);
+            this.styleDiv.appendChild(this.li)
+        }
+
+        let small = this.styleDiv.getElementsByClassName('adsmall_wrap');
+        while (small[dataDop.length]) {
+            this.styleDiv.removeChild(small[dataDop.length]);
+        }
+
+    }.bind(this)
+
+    this.ul.onclick = this.menuList;
+
+    this.changePosition = function () {
+        if (this.ul.classList.contains('adwrap_block')) {
+            this.ul.className = 'advertical_block';
+        } else {
+            this.ul.className = 'adwrap_block';
+        }
+    }.bind(this)
+
+    this.changeAddItems = function () {
+        if (this.styleDiv.classList.contains('adstyle_block')) {
+            this.styleDiv.className = '';
+        } else {
+            this.styleDiv.className = 'adstyle_block';
+        }
+    }.bind(this)
+
+    this.button.onclick = this.changePosition;
+    this.button2.onclick = this.changeAddItems;
+}
+
+let winInMid = window.innerWidth
+
+function screenResolutionCheck (winInMid) {
+    if (winInMid > 560) {
+        new Burger(menuArr, dopMenu, '#menu_block');
+    } else {
+        new AdaptiveBurger (menuArr, dopMenu, '#menu_block')
+    }
+}
+
+screenResolutionCheck();
